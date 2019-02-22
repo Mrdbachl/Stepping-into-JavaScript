9d54d87cc7868a9f337ddb5046b4ff0d577e4ed4
Mrdbachl <cr60722@ravens.racc.edu>
2019-02-22 05:36:59 -0700

    wrote extra functions to understand using variables better.

--- test.js
+++ test.js
@@ -6,5 +6,25 @@ function getAverage(a,b){
 	
 }
 	
-var myResult = getAverage(7,10);	
+var myResult = getAverage(20,10);	
 console.log("the average is " + myResult);
+
+function highAverage(c,d,e){
+	
+	var newAverage = (myResult + c + d + e) / 5;
+	console.log (newAverage);
+	return newAverage;
+	
+}
+
+var fiveNumbers = highAverage(10,20,10);
+console.log("this average is " + fiveNumbers);
+
+function difference(){
+	
+	var averageDif = (myResult - fiveNumbers);
+	return averageDif;
+}
+
+var subtraction = difference();
+console.log("the difference is " + subtraction);
